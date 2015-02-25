30772957dad2dc9b68ea8ea996ad8ec0cd9ccccc
mscienski <michal.scienski@idfl.com>
2015-02-24 21:49:01 -0700

    Lab 1
    Create Lab 1

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, my name is Michal");
+            Console.WriteLine("My best friend is Anna");
+            Console.WriteLine("My favorite games are: ");
+            Console.Write("Middle Earth: Shadow of Mordor\nHalf Life 2\nRed Alert 2");
+            Console.WriteLine("\n");
+        }
+    }
+}
