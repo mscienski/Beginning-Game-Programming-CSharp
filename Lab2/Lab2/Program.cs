4c5040c5e8ae9dfa2fb15e46f3efd1b1fc612886
mscienski <michal.scienski@idfl.com>
2015-02-24 21:56:52 -0700

    Lab 2
    Make Lab 2

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            const int MAX_SCORE = 100;
+            int score = 97;
+            float percent = (float)score/(float)MAX_SCORE * 100;
+            age = 29;
+            Console.WriteLine("My age is: " + age.ToString("N0"));
+            Console.WriteLine("The score percent is: " + percent.ToString("F2"));
+        }
+    }
+}
