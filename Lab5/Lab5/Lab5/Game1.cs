e960b26c9a631c979e6a8e122d2944159743462c
mscienski <mscienski@gmail.com>
2015-03-04 00:02:55 -0700

    Labs
    Do labs 4, 5, and 6.

---
+++ Lab5/Lab5/Lab5/Game1.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Lab5
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        const int WINDOW_WIDTH = 800;
+        const int WINDOW_HEIGHT = 600;
+
+        Texture2D pic1;
+        Texture2D pic2;
+        Texture2D pic3;
+
+        Rectangle rec1;
+        Rectangle rec2;
+        Rectangle rec3;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferHeight = WINDOW_HEIGHT;
+            graphics.PreferredBackBufferWidth = WINDOW_WIDTH;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            pic1 = Content.Load<Texture2D>("next");
+            rec1 = new Rectangle(150, 100, pic1.Width, pic1.Height);
+
+            pic2 = Content.Load<Texture2D>("previous");
+            rec2 = new Rectangle(250, 100, pic2.Width, pic2.Height);
+
+            pic3 = Content.Load<Texture2D>("first");
+            rec3 = new Rectangle(350, 100, pic3.Width, pic3.Height);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // draw teddy bears
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(pic1, rec1, Color.White);
+            spriteBatch.Draw(pic2, rec2, Color.White);
+            spriteBatch.Draw(pic3, rec3, Color.White);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
