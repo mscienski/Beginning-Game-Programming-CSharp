79325d5e21432583255968b190f8dae79fe7695b
mscienski <mscienski@gmail.com>
2015-03-29 14:20:00 -0600

    Programming Assignment 4
    Update with programming assignment 4

---
+++ Week5/ProgrammingAssignment4/ProgrammingAssignment4/Game1.cs
@@ -0,0 +1,192 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace ProgrammingAssignment4
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        const int WINDOW_WIDTH = 800;
+        const int WINDOW_HEIGHT = 600;
+
+        // teddy support
+        Texture2D teddySprite;
+        Teddy teddy;
+
+        // pickup support
+        Texture2D pickupSprite;
+        List<Pickup> pickups = new List<Pickup>();
+
+        // click processing
+        bool rightClickStarted = false;
+        bool rightButtonReleased = true;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            // STUDENTS: set resolution and make mouse visible
+            graphics.PreferredBackBufferWidth = WINDOW_WIDTH;
+            graphics.PreferredBackBufferHeight = WINDOW_HEIGHT;
+            IsMouseVisible = true;
+
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // STUDENTS: load teddy and pickup sprites
+            teddySprite = Content.Load<Texture2D>("teddybear");
+            pickupSprite = Content.Load<Texture2D>("pickup");
+
+
+            // STUDENTS: create teddy object centered in window
+            teddy = new Teddy(teddySprite, new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2));
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // STUDENTS: get current mouse state and update teddy
+            MouseState mouse = Mouse.GetState();
+            teddy.Update(gameTime, mouse);
+
+            // check for right click started
+            if (mouse.RightButton == ButtonState.Pressed &&
+                rightButtonReleased)
+            {
+                rightClickStarted = true;
+                rightButtonReleased = false;
+            }
+            else if (mouse.RightButton == ButtonState.Released)
+            {
+                rightButtonReleased = true;
+
+                // if right click finished, add new pickup to list
+                if (rightClickStarted)
+                {
+                    rightClickStarted = false;
+
+                    // STUDENTS: add a new pickup to the end of the list of pickups
+                    pickups.Add(new Pickup(pickupSprite, new Vector2(mouse.X, mouse.Y)));
+
+
+                    // STUDENTS: if this is the first pickup in the list, set teddy target
+                    if (pickups.Count == 1)
+                    {
+                        teddy.SetTarget(new Vector2(pickups[0].CollisionRectangle.X, pickups[0].CollisionRectangle.Y));
+                    }
+
+                }
+            }
+
+            // STUDENTS: Delete the line saying if (true) and uncomment the three
+            // lines below that AFTER you've created a teddy object in the
+            // LoadContent method
+            // check for collision between collecting teddy and targeted pickup
+            //if (true)
+            if (teddy.Collecting &&
+                 pickups.Count > 0 &&
+                 teddy.CollisionRectangle.Intersects(pickups[0].CollisionRectangle))
+            {
+                // STUDENTS: remove targeted pickup from list (it's always at location 0)
+                pickups.RemoveAt(0);
+
+                // STUDENTS: if there's another pickup to collect, set teddy target
+                // If not, stop the teddy from collecting
+                if (pickups.Count > 0)
+                {
+                    teddy.SetTarget(new Vector2(pickups[0].CollisionRectangle.X, pickups[0].CollisionRectangle.Y));
+                }
+                else
+                {
+                    teddy.Collecting = false;
+                }
+                teddy.Update(gameTime, mouse);
+
+            }
+            else if (pickups.Count == 0)
+            {
+                //also stop collecting if no pickups remain and the other conditions are not met
+                //see https://class.coursera.org/gameprogramming-002/forum/thread?thread_id=1314
+                teddy.Collecting = false;
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // draw game objects
+            spriteBatch.Begin();
+
+            // STUDENTS: Uncomment the following line AFTER you create
+            // a teddy object in the LoadContent method
+            teddy.Draw(spriteBatch);
+            foreach (Pickup pickup in pickups)
+            {
+                pickup.Draw(spriteBatch);
+            }
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Week5/ProgrammingAssignment4/ProgrammingAssignment4/Pickup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ProgrammingAssignment4
+{
+    /// <summary>
+    /// A pickup
+    /// </summary>
+    public class Pickup
+    {
+        #region Fields
+
+        // drawing support
+        Texture2D sprite;
+        Rectangle drawRectangle;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="sprite">sprite for the pickup</param>
+        /// <param name="location">location of the center of the pickup</param>
+        public Pickup(Texture2D sprite, Vector2 location)
+        {
+            this.sprite = sprite;
+
+            // STUDENTS: set draw rectangle so pickup is centered on location
+            this.drawRectangle = new Rectangle((int)location.X - this.sprite.Width / 2, (int)location.Y - this.sprite.Height, this.sprite.Width, this.sprite.Height);
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the collision rectangle for the pickup
+        /// </summary>
+        public Rectangle CollisionRectangle
+        {
+            get { return drawRectangle; }
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Draws the pickup
+        /// </summary>
+        /// <param name="spriteBatch">sprite batch</param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // STUDENTS: use the sprite batch to draw the pickup
+            spriteBatch.Draw(this.sprite, this.drawRectangle, Color.White);
+        }
+
+        #endregion
+    }
+}
